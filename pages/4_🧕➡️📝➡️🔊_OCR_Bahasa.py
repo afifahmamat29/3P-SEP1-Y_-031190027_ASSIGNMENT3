517decbd9633a4a52b357c4f5bddbaa2c481761f
afifahmamat29 <52043470+afifahmamat29@users.noreply.github.com>
2023-03-13 21:58:55 +0800

    Update 4_🧕➡️📝➡️🔊_OCR_Bahasa.py

--- pages/4_🧕➡️📝➡️🔊_OCR_Bahasa.py
+++ pages/4_🧕➡️📝➡️🔊_OCR_Bahasa.py
@@ -40,7 +40,7 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 reader = easyocr.Reader(['en','ms','fr']) # this needs to run only once to load the model into memory
 
 
-st.title('OCR Application')
+st.title('Bahasa OCR Application')
 
 #for audio(gTTS)
 lang_dict = {'Malay':'ms','English':'en','French':'fr'}
