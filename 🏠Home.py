3c8c9a54b0896185e1daad3af6cc3e63fe1b4904
afifahmamat29 <52043470+afifahmamat29@users.noreply.github.com>
2023-03-14 12:25:35 +0800

    Update 🏠Home.py

--- 🏠Home.py
+++ 🏠Home.py
@@ -43,33 +43,33 @@ col1, col2, col3 = st.columns(3)
 
 with col1:
    st.header("TTS")
-   st.markdown('''<a href="https://afifahmamat29-3p-sep1-y--031190027-assignment3-home-t8pyu1.streamlit.app/%E2%9E%A1%EF%B8%8F%F0%9F%94%8A_Text_App"> 
+   st.markdown('''<a href="https://afifah-se-project.streamlit.app/%E2%9E%A1%EF%B8%8F%F0%9F%94%8A_Text_App"> 
    <img src="https://raw.githubusercontent.com/afifahmamat29/WOU-SE-PROJECT/main/img/text%20to%20speech(transparent).png"  width="100%" height="100%" text-align: center;/></a>'''\
                ,unsafe_allow_html=True)
    
 with col2:
    st.header("OCR")
-   st.markdown('''<a href="https://afifahmamat29-3p-sep1-y--031190027-assignment3-home-t8pyu1.streamlit.app/%E2%9E%A1%EF%B8%8F%F0%9F%93%9D%E2%9E%A1%EF%B8%8F%F0%9F%94%8A_OCR_App"> 
+   st.markdown('''<a href="https://afifah-se-project.streamlit.app/%E2%9E%A1%EF%B8%8F%F0%9F%93%9D%E2%9E%A1%EF%B8%8F%F0%9F%94%8A_OCR_App"> 
    <img src="https://raw.githubusercontent.com/afifahmamat29/WOU-SE-PROJECT/main/img/OCR.png"  width="100%" height="100%" /></a>'''\
                ,unsafe_allow_html=True)
    s_col1, s_col2, s_col3 = st.columns(3)
    with s_col1:
-      st.markdown('''<a href="https://afifahmamat29-3p-sep1-y--031190027-assignment3-home-t8pyu1.streamlit.app/%E2%9E%A1%EF%B8%8F%F0%9F%93%9D%E2%9E%A1%EF%B8%8F%F0%9F%94%8A_OCR_Bahasa"> 
+      st.markdown('''<a href="https://afifah-se-project.streamlit.app/%E2%9E%A1%EF%B8%8F%F0%9F%93%9D%E2%9E%A1%EF%B8%8F%F0%9F%94%8A_OCR_Bahasa"> 
    <img src="https://raw.githubusercontent.com/afifahmamat29/WOU-SE-PROJECT/main/img/1.png"  width="100%" height="100%" /></a>'''\
                ,unsafe_allow_html=True)
    with s_col2:
-      st.markdown('''<a href="https://afifahmamat29-3p-sep1-y--031190027-assignment3-home-t8pyu1.streamlit.app/%E2%9E%A1%EF%B8%8F%F0%9F%93%9D%E2%9E%A1%EF%B8%8F%F0%9F%94%8A_OCR_Mandarin"> 
+      st.markdown('''<a href="https://afifah-se-project.streamlit.app/%E2%9E%A1%EF%B8%8F%F0%9F%93%9D%E2%9E%A1%EF%B8%8F%F0%9F%94%8A_OCR_Mandarin"> 
    <img src="https://raw.githubusercontent.com/afifahmamat29/WOU-SE-PROJECT/main/img/2.png"  width="100%" height="100%" /></a>'''\
                ,unsafe_allow_html=True)
    with s_col3:
-      st.markdown('''<a href="https://afifahmamat29-3p-sep1-y--031190027-assignment3-home-t8pyu1.streamlit.app/%E2%9E%A1%EF%B8%8F%F0%9F%93%9D%E2%9E%A1%EF%B8%8F%F0%9F%94%8A_OCR_Tamil"> 
+      st.markdown('''<a href="https://afifah-se-project.streamlit.app/%E2%9E%A1%EF%B8%8F%F0%9F%93%9D%E2%9E%A1%EF%B8%8F%F0%9F%94%8A_OCR_Tamil"> 
    <img src="https://raw.githubusercontent.com/afifahmamat29/WOU-SE-PROJECT/main/img/3.png"  width="100%" height="100%" /></a>'''\
                ,unsafe_allow_html=True)
    
 
 with col3:
    st.header("PDF To Speech")
-   st.markdown('''<a href="https://afifahmamat29-3p-sep1-y--031190027-assignment3-home-t8pyu1.streamlit.app/%E2%9E%A1%EF%B8%8F%F0%9F%94%8A_PDF_App"> 
+   st.markdown('''<a href="https://afifah-se-project.streamlit.app/%E2%9E%A1%EF%B8%8F%F0%9F%94%8A_PDF_App"> 
    <img src="https://raw.githubusercontent.com/afifahmamat29/WOU-SE-PROJECT/main/img/PDFTOSPEECH.png"  width="100%" height="100%" /></a>'''\
                ,unsafe_allow_html=True)
    
